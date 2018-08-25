c89c99a7c1cda03a46113aeb329b5ff239b9cbc1
Your Name <you@example.com>
2018-08-25 11:38:38 +0800

    shi yan

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180824092641) do
+ActiveRecord::Schema.define(version: 20180825033616) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "wage_upper_bound"
+    t.integer  "wage_lower_bound"
+    t.string   "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
